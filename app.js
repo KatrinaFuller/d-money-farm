26c5ab2aeeb35a4d0a15d5511f5909532ff9a72c
Katrina Fuller <trinafuller@att.net>
2019-08-15 11:26:02 -0600

    done

--- app.js
+++ app.js
@@ -1,15 +1,45 @@
-let justCorn = {
-  name: " Just Corn",
-  worth: 100,
-  waterCount: 0,
+
+let farm = {
+  money: 0,
+  corn: {
+    name: " Just Corn",
+    worth: 100,
+    waterCount: 0,
+  },
+  celery: {
+    name: " Celery",
+    worth: 5,
+    waterCount: 0,
+  },
 }
 
-let celery = {
-  name: " Celery",
-  worth: 5,
-  waterCount: 0,
+function water(cropName) {
+  let crop = farm[cropName];
+  crop.waterCount++
+  draw();
 }
 
-function water() {
+function harvest(cropName) {
+  let crop = farm[cropName];
+  farm.money += crop.worth * crop.waterCount;
+  crop.waterCount = 0;
 
+  // if (crop === "corn") {
+  //   money += justCorn.worth * justCorn.waterCount;
+  //   justCorn.waterCount = 0;
+  // } else {
+  //   money += celery.worth * celery.waterCount;
+  //   celery.waterCount = 0;
+  // }
+  draw();
+}
+
+function draw() {
+  let moneyFieldElement = document.querySelector("#money-field");
+  let cornWaterElement = document.querySelector("#corn-water");
+  let celeryWaterElement = document.querySelector("#celery-water");
+
+  moneyFieldElement.textContent = `Money: $ ${farm.money}`;
+  cornWaterElement.textContent = `Watered: ${farm.corn.waterCount}`;
+  celeryWaterElement.textContent = `Watered: ${farm.celery.waterCount}`;
 }
